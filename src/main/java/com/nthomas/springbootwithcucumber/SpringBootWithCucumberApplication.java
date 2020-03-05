91665a3b1630873138ce6c73bb70651ef3c3487b
Nathan Thomas <nate.p.thomas@gmail.com>
2020-03-05 16:29:49 -0500

    Initial commit

---
+++ src/main/java/com/nthomas/springbootwithcucumber/SpringBootWithCucumberApplication.java
@@ -0,0 +1,13 @@
+package com.nthomas.springbootwithcucumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithCucumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithCucumberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nthomas/springbootwithcucumber/SpringBootWithCucumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nthomas.springbootwithcucumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithCucumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
